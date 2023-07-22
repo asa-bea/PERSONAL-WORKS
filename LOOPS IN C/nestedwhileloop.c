3c54d96a6878c7d41490797b5b04da6282f92f18
asa-bea <akosasab@yahoo.com>
2023-07-22 09:17:39 +0000

    nested whille and for loop examples

---
+++ LOOPS IN C/nestedwhileloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int i = 0, j = 1;
+    while (i < 3)
+    {
+        while (j <= 3)
+        {
+            printf("%d", j++);
+        }
+        printf("%d", i++);
+    }
+    
+}
